1d5b62b26309510dd98174a7fad4b8341b76a531
MarcosViniciusPeres <mvperes95@gmail.com>
2021-09-27 10:08:37 -0300

    Create estadotabuleiroGA class

---
+++ EstadoTabuleiroGA.py
@@ -0,0 +1,97 @@
+import random
+
+class EstadoTabuleiroGA:
+    tabuleiro = []
+    nRainhas = 0 
+    valorFitness = 0 
+    propLocal = 0
+    percAptidao = 0
+
+    def getPropLocal(self):
+        return self.propLocal
+    
+    def setPropLocal(self, propLocal):
+        self.propLocal = propLocal
+
+    def getPercAptidao(self):
+        return self.percAptidao
+
+    def setPercAptidao(self, percAptidao):
+        self.percAptidao = percAptidao
+    
+    def setValorFitness(self, valorFitness):
+        self.valorFitness = valorFitness
+
+    def getValorFitness(self):
+        return self.valorFitness
+    
+    def setTabuleiro(self, tabuleiro):
+        self.tabuleiro = tabuleiro
+
+    def getTabuleiro(self):
+        return self.tabuleiro
+
+    def __init__(self, qtdeRainhas):
+        self.nRainhas = qtdeRainhas
+
+        self.tabuleiro = []
+    
+        for linha in range(self.nRainhas):
+            linhas = []
+            for coluna in range(self.nRainhas):
+                if linha == coluna:
+                    linhas.append(True)
+                else:
+                    linhas.append(False)
+            self.tabuleiro.append(linhas)
+        
+        for linha in range(self.nRainhas):
+            self.trocaPosicaoLinhas(linha, random.randint(0,  (self.nRainhas-1)))
+
+        self.funcaoFitness();
+
+
+    def funcaoFitness(self):
+        fitnessParcial = 0 
+        for linha in range(self.nRainhas):
+            for coluna in range(self.nRainhas):
+                if self.tabuleiro[linha][coluna]:
+                    fitnessParcial += self.validarColisao(linha, coluna)
+        self.valorFitness = (fitnessParcial / 2)
+
+    def validarColisao(self, linhaRainhaOrigem, colunaRainhaOrigem):
+        qtdeConflitos = 0
+        for linhaRainhaAtual in range(self.nRainhas):
+            for colunaRainhaAtual in range(self.nRainhas):
+                
+                temRainha = self.tabuleiro[linhaRainhaAtual][colunaRainhaAtual]
+                
+                naoEhRainhaOrigem = (linhaRainhaAtual  != linhaRainhaOrigem and colunaRainhaAtual != colunaRainhaOrigem)
+                
+                estaNaLinhaColunaRainhaOrigem = (linhaRainhaAtual  == linhaRainhaOrigem or colunaRainhaAtual == colunaRainhaOrigem)
+
+                estaNaDiagonalRainhaOrigem = (linhaRainhaAtual + colunaRainhaAtual == linhaRainhaOrigem + colunaRainhaOrigem or linhaRainhaAtual - colunaRainhaAtual == linhaRainhaOrigem - colunaRainhaOrigem)
+
+                if temRainha and ((naoEhRainhaOrigem and estaNaDiagonalRainhaOrigem) or (estaNaLinhaColunaRainhaOrigem and not naoEhRainhaOrigem and not estaNaDiagonalRainhaOrigem)):
+                    qtdeConflitos += 1
+									
+		return qtdeConflitos			            
+						         
+    def trocaPosicaoLinhas(self, n1, n2):
+            linha = self.tabuleiro[n1]
+            self.tabuleiro[n1] = self.tabuleiro[n2]
+            self.tabuleiro[n2] = linha
+   
+    def imprimirTabuleiro(self):
+        print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+        print("Impressao do resultado:")
+        for linha in range(self.nRainhas):
+            for coluna in range(self.nRainhas):
+                print(" R " if self.tabuleiro[linha][coluna] == True else " - ", end="")
+            print("")
+
+        print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+
+        print("Solucao otima encontrada!" if self.energia == 0 else "Solucao otima nao encontrada!")
+
+
---
+++ GeneticAlgorithm.py
@@ -0,0 +1,70 @@
+import time
+import sys
+
+def tempoAtualMili():
+    return round(time.time() * 1000)
+
+def populaValoresValidosQtdeCruzamento(valoresValidosQtdeCruzamento, qtdePopInicial):
+    valorAdimissivel = int((qtdePopInicial/2))
+    for valorAdimissivel in range(valorAdimissivel,qtdePopInicial):
+        if valorAdimissivel % 2 == 0 and ((valorAdimissivel + 2) <= qtdePopInicial):
+            valoresValidosQtdeCruzamento.append(valorAdimissivel)
+
+def geraPopInicial(self, qtdeRainhas, tamanhoPopInicial):
+    listEstados = []
+
+    for i in range(qtdePopInicial):
+        novoEstado = EstadoTabuleiroGA(qtdeRainhas)    
+        listEstados.append(novoEstado)
+
+    return listEstados
+
+tempoInicial = 0    
+qtdeRainhas = 0 
+qtdeGeracoes = 0
+qtdePopInicial = 0
+qtdeMaxSelecao = 0
+valoresValidosQtdeCruzamento = []
+
+def main():
+    global qtdeRainhas, qtdeGeracoes, qtdePopInicial, qtdeMaxSelecao
+    while True:
+        
+        valoresValidosQtdeCruzamento = []
+
+        qtdeRainhas = int(input("Informe a quantidade de Rainhas(>=4): "))
+
+        if qtdeRainhas < 4:
+            print("A quantidade de rainhas informada é inválida!")
+            print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+            continue
+        
+        qtdePopInicial = int(input("Informe quantos indivíduos haverá na população inicial(>= 4 e <=10): "))
+
+        if qtdePopInicial < 4 or qtdePopInicial > 10:
+            print("A quantidade de indivíduos na população inicial é inválida!")
+            print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+            continue
+
+        populaValoresValidosQtdeCruzamento(valoresValidosQtdeCruzamento, qtdePopInicial)
+
+        print("Valores Validos são: ", valoresValidosQtdeCruzamento)
+        qtdeMaxSelecao = int(input("Informe quantos indivíduos serão selecionados para cruzamento: "))
+
+        if not qtdeMaxSelecao in valoresValidosQtdeCruzamento:
+            print("A quantidade de indivíduos selecionados para cruzamento é inválida!")
+            print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+            continue
+        
+        qtdeGeracoes = int(input("Informe quantas gerações de indivíduos devem ser gerados: "))
+
+        popInicial = []
+
+        MAX_INT = sys.maxsize
+
+        melhorFilho = melhorIndividuoAteAgora(geraPopInicial(qtdeRainhas, qtdePopInicial), MAX_INT)
+
+        print("A execucao levou ", int((tempoAtualMili() - tempoInicial) / 1000), " segundos(s).")
+        print("\n\n--------------------------------------------------------------------------------------------------------------------\n\n")
+
+main()
